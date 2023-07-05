1fb5f25113b08b8ef05588d911cc04fe6ea0dbdb
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-07-05 23:13:57 +0300

    add and create card view

---
+++ AppleWallet/AppleWallet/ViewModel/TransactionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionViewModel.swift
+//  AppleWallet
+//
+//  Created by Ahmed Amin on 05/07/2023.
+//
+
+import Foundation
--- AppleWallet/AppleWallet/Views/CardView.swift
+++ AppleWallet/AppleWallet/Views/CardView.swift
@@ -8,13 +8,64 @@
 import SwiftUI
 
 struct CardView: View {
+    
+    //MARK: - Attributes
+    
+    private var card: Card
+    
+    //MARK: - Init
+    
+    init(card: Card) {
+        self.card = card
+    }
+    
+    //MARK: - Body
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Image(card.image)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .overlay(cardContent, alignment: .bottomLeading)
+            .shadow(color: .gray,
+                    radius: 1.0, x: 0, y: 1.0)
+    }
+}
+
+//MARK: - SETUP VIEW
+
+extension CardView {
+    
+    private var cardContent: some View {
+        
+        VStack(alignment: .leading) {
+            
+            Text(card.number)
+                .font(.system(.title3, design: .rounded))
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            
+            HStack {
+                
+                Text(card.name)
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+                
+                Text("Valid Thru")
+                    .font(.system(.footnote, design: .rounded))
+                
+                Text(card.expiryDate)
+                    .font(.system(.footnote, design: .rounded))
+            }
+            .foregroundColor(.white)
+            
+        }
+        .padding()
     }
 }
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView()
+        CardView(card: dev.cards[0])
     }
 }
