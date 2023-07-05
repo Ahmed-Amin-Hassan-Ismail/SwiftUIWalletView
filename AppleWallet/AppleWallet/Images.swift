dc53c4216d7c7c39a57624989fddf263702c32d8
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-07-05 22:38:12 +0300

    add asset images

---
+++ AppleWallet/AppleWallet/Images.swift
@@ -0,0 +1,19 @@
+//
+//  Images.swift
+//  AppleWallet
+//
+//  Created by Ahmed Amin on 05/07/2023.
+//
+
+import Foundation
+
+
+
+enum AssetImages: String, CaseIterable {
+    
+    case master = "master"
+    case visa = "visa"
+    case discover = "discover"
+    case chase = "chase"
+    case americanExpress = "ae"
+}
