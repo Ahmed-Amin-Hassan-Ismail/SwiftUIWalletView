9a8027039d610f55388d417460d8b9108b5ac51b
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-07-05 22:33:00 +0300

    initialize Xcode project

---
+++ AppleWallet/AppleWallet/AppleWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWalletApp.swift
+//  AppleWallet
+//
+//  Created by Ahmed Amin on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
